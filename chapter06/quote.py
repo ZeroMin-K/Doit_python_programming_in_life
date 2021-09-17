c80097365b7337de1e6b54e0b6faa43d596abe01
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-09-17 23:48:43 +0900

    Create quote.py

---
+++ chapter06/quote.py
@@ -0,0 +1,24 @@
+import os, re
+from typing import ByteString
+import requests
+import urllib.request as ur 
+from bs4 import BeautifulSoup as bs
+
+url = 'https://quotes.toscrape.com/'
+
+# url 객체에 저장된 URL주소에 해당하는 웹상트 불러오기 
+# html = ur.urlopen(url)
+# soup = bs(html.read(), 'html.parser')
+
+# 위 내용을 한 줄로 만들기 
+soup = bs(ur.urlopen(url).read(), 'html.parser')
+
+# span 태그 찾기 
+quote = soup.find_all('span')
+
+# 찾은 태그에서 텍스트만 출력
+print(quote[0].text)
+
+# 반복문을 이용해 찾은 태그들의 텍스트 전부 출력. 
+for i in quote:
+    print(i.text)
